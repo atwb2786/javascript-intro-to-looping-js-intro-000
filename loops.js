0db9b31aa529c73acc4c9551b9de5eb4be8e645e
punctual-sailor-2214 <bchett286@yahoo.com>
2019-01-27 17:26:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,13 +20,13 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  let i = 0;
+  var i = 0;
   function incrementVariable() {
     i = i + 1;
   }
   do {
     i++;
     console.log(i);
-  } while (false);
+  } while (array.length > 0 && incrementVariable());
   return array;
 }
