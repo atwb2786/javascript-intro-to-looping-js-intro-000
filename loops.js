0fc636bdb20d4be762be21b02c03bd6563a5458a
punctual-sailor-2214 <bchett286@yahoo.com>
2019-01-27 17:55:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -26,6 +26,6 @@ function doWhileLoop(array) {
   }
   do {
     array.pop;
-  } while (array.length > 1 && incrementVariable());
+  } while (array.length > 0 && incrementVariable());
   return array;
 }
